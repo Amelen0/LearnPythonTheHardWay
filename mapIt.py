6a7da5b4849812f564359b4ef3217ea81e5ec6ce
AbdielMelendez/ <abdielhmelendez@gmail.com>
2015-11-11 10:25:41 -0800

    Updating

--- mapIt.py
+++ mapIt.py
@@ -2,7 +2,10 @@
 # mapIt.py
 # command line or clipboard
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
   # Get address from the command line
   address = ' '.join(sys.argv[1:])
+else:
+  # Get address from the clipboard
+  address = pyperclip.paste()
